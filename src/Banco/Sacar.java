9c7efcbe4559ba2f85ef0a3d84a2b5a4f239d75b
Lucas Neves <lucas54neves@gmail.com>
2018-11-08 19:13:30 -0200

    Criação da classe Sacar

---
+++ src/Banco/Sacar.java
@@ -0,0 +1,29 @@
+package Banco;
+
+import java.util.Date;
+
+public class Sacar {
+    private final Conta mConta;
+    private final long mData;
+    private final double mSaldoAnterior;
+    private final double mValor;
+
+    public Sacar(Conta conta, double valor) {
+        this.mConta = conta;
+        this.mData = new Date().getTime();
+        this.mSaldoAnterior = conta.GetSaldo();
+        this.mValor = valor;
+        this.mConta.Sacar(this.mValor);
+    }
+    
+    @Override
+    public String toString() {
+        return "# Saque #"+
+                "\nAgencia: "+mConta.GetAgencia()+
+                "\nConta: "+mConta.GetConta()+
+                "\nData: "+new Date(mData)+
+                "\nSaldo anterior: "+mSaldoAnterior+
+                "\nSaque: "+mValor+
+                "\nSaldo atual: "+mConta.GetSaldo();
+    }
+}
