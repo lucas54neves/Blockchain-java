f3399982eb7c1f84139000b39f59cd79ff7bafbf
Lucas Neves <lucas54neves@gmail.com>
2018-11-15 12:01:57 -0200

    Atualização da classe Sacar
    Aperfeiçoamento da condição para saque insuficiente.

--- src/Banco/Sacar.java
+++ src/Banco/Sacar.java
@@ -10,15 +10,26 @@ public class Sacar {
 
     public Sacar(Conta conta, double valor) {
         this.mConta = conta;
-        this.mData = new Date().getTime();
         this.mSaldoAnterior = conta.GetSaldo();
         this.mValor = valor;
-        this.mConta.Sacar(this.mValor);
+        
+        if (this.mConta.GetSaldo() >= this.mValor) {
+            this.mConta.Sacar(this.mValor);
+            this.mData = new Date().getTime();
+        } else {
+            this.mData = 0; // Seria essa a melhor opção?
+            System.out.println("Saque inválido.");
+            System.out.println("O saldo da conta é insuficiente.");
+        }
+    }
+    
+    public Conta GetConta() {
+        return mConta;
     }
     
     @Override
     public String toString() {
-        return "\nAgencia: "+mConta.GetAgencia()+
+        return "Agencia: "+mConta.GetAgencia()+
                "\nConta: "+mConta.GetConta()+
                "\nData: "+new Date(mData)+
                "\nSaldo anterior: "+mSaldoAnterior+
